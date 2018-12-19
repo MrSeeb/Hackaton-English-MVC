25e34c37f09077ed9bf67b6f4a1f1b6ddb7097c3
Nassim <nassim19961996@outlook.fr>
2018-12-19 10:46:30 +0100

    Modular function

--- config/global.php
+++ config/global.php
@@ -3,13 +3,9 @@
 function getGlobalConfig() {
   return $config = [
     "protocol" => "",
-<<<<<<< HEAD
     "host" => "localhost/Lab/Hackaton-English-MVC/",
-    "status" => ["user", "secretary", "teacher", "admin"],
-=======
     "host" => "localhost:8888/Lab/Hackaton-English-MVC/",
     "status" => ["Secretary", "user", "Teacher"],
->>>>>>> 26c5c53009e8f676088ed5a7753b070ed9213742
     "defaultRoute" => ""
   ];
 }
--- model/session.php
+++ model/session.php
@@ -11,7 +11,7 @@ function getSessions()
 // function to have one session
 function getSession($user_id)
 {
-  $db = getDataBase();
+  $db = connectToDataBAse();
   $query = $db->prepare("SELECT * FROM session WHERE user_id=?");
   $query->execute([$user_id]);
   $session = $query->fetch(PDO::FETCH_ASSOC);
@@ -22,7 +22,7 @@ function getSession($user_id)
 // function ok for user_id and created_date
 function addSession($session, $user_id, $code)
 {
-  $db = getDataBase();
+  $db = connectToDataBAse();
   $query = $db->prepare("INSERT INTO session (user_id, code, created_date, start_qcm_date, end_qcm_date, result, level) VALUES(:user_id, :code, CURDATE(), :start_qcm_date, :end_qcm_date, :result, :level)");
   $result = $query->execute([
     "user_id" => $user_id,
