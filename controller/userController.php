181ba7de4f39ad297310e27e1d8493512b281341
Sebastien <sebastien.munoz@outlook.fr>
2018-12-19 09:20:54 +0100

    update structure and files

--- controller/userController.php
+++ controller/userController.php
@@ -6,7 +6,7 @@ require "view/indexView.php";
 
 function loginStudent()
 {
-    $reponses = getDataBase()->query('SELECT u.last_name, u.first_name, s.code FROM user u INNER JOIN session s ON u.id_user = s.user_id ');
+    $reponses = getDataBase()->query('SELECT u.last_name, u.first_name, s.code FROM user u INNER JOIN session s ON u.id_user = s.user_id WHERE u.id = ?');
     $reponse = $reponses->fetchall();
     //Check if the form is completed
     if(!empty($_POST))
@@ -14,19 +14,19 @@ function loginStudent()
         //clear the form enter
         $_POST = clearForm($_POST);
         //Collect the stored users
-        foreach ($reponse as $key =>$user)
-        {
-            if($user["first_name"] === $_POST["first_name"] && $user["last_name"] === $_POST["last_name"] && $user["code"] === $_POST["code"])
-            {
-                //Start a session to store the user information stored session
-                session_start();
-                $session = $_SESSION["session"]; 
-                //Start a session to store the user information stored answers
-                $reponseStudent = $_SESSION["reponse"];
-                redirectTo("bravo");
-            }
-        }
-        redirectTo("");
+        // foreach ($reponse as $key =>$user)
+        // {
+        //     if($user["first_name"] === $_POST["first_name"] && $user["last_name"] === $_POST["last_name"] && $user["code"] === $_POST["code"])
+        //     {
+        //         //Start a session to store the user information stored session
+        //         session_start();
+        //         $session = $_SESSION["session"]; 
+        //         //Start a session to store the user information stored answers
+        //         $reponseStudent = $_SESSION["reponse"];
+        //         redirectTo("bravo");
+        //     }
+        // }
+        // redirectTo("");
     }
 }
 ?>
